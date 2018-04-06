147b5151365686bafcd5a87564d7f01a955c3e04
Delagardi <delagardi@redy.io>
2018-04-06 05:40:28 +0300

    UPDATE method done.

--- app/routes/renters_routes.js
+++ app/routes/renters_routes.js
@@ -1,10 +1,26 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(application, db) {
+  // PUT METHOD
+  application.put("/renters/:id", (req, res) => {
+    const id = req.params.id;
+    const details = {"_id": new ObjectID(id)};
+    const renter = {company: req.body.company, contacts: req.body.contacts};
+
+    db.collection("renters").update(details, renter, (err, result) => {
+      if (err) {
+        res.send({"error":"You get an error with UPDATE method"})
+      } else {
+        res.send(renter);
+      }
+    });
+  });
+
   // DELETE METHOD
   application.delete("/renters/:id", (req, res) => {
     const id = req.params.id;
     const details = {"_id": new ObjectID(id)};
+
     db.collection("renters").remove(details, (err, item) => {
       //error catching
       if (err) {
@@ -19,6 +35,7 @@ module.exports = function(application, db) {
   application.get("/renters/:id", (req, res) => {
     const id = req.params.id;
     const details = {"_id": new ObjectID(id)};
+
     db.collection("renters").findOne(details, (err, item) => {
       //error catching
       if (err) {
@@ -29,14 +46,14 @@ module.exports = function(application, db) {
     });
   });
 
-  //POST METHOD
+  // POST METHOD
   application.post("/renters", (req, res) => {
-    const renter = { company: req.body.company, contacts: req.body.contacts };
+    const renter = {company: req.body.company, contacts: req.body.contacts};
 
     db.collection("renters").insert(renter, (err, result) => {
       //error catching
       if (err) {
-        res.send({ "error": "You get an error with POST method"});
+        res.send({"error": "You get an error with POST method"});
       } else {
         res.send(result.ops[0]);
       }
