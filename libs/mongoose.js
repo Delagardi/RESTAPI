4c76ae4193591b21e19f97ab80c8e104107bd7f2
Delagardi <delagardi@redy.io>
2018-04-10 06:47:04 +0300

    deleting all

--- libs/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-//const config = require("../config");
-
-const uriDB = "mongodb://localhost/renters"
-
-mongoose.connect("uriDB");
-
-module.exports = mongoose;
