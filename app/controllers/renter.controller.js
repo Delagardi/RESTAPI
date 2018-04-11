f6b4310a9b42fb761f8d198fabc5efd84ba13c0d
Delagardi <delagardi@redy.io>
2018-04-11 07:00:14 +0300

    All methdos now in DB.js file and working

--- app/controllers/renter.controller.js
+++ app/controllers/renter.controller.js
@@ -20,43 +20,17 @@ exports.create = (req, res) => {
   });
 
   // Saving renter in Database
-
   dbOperation.saveDB(renter, res);
-
-  // renter.save().then(data => {
-  //   res.send(data);
-  // }).catch(err => {
-  //   res.status(500).send({
-  //     message: err.message
-  //   });
-  // });
 };
 
 //Finding all renters
 exports.findAll = (req, res) => {
-  Renter.find().then(renters => {
-    res.send(renters);
-  }).catch(err => {
-    res.status(500).send({
-      message: err.message
-    });
-  });
+  dbOperation.findAllEntrys(Renter, res);
 };
 
 //Finding one renter by ID
-exports.findOne = (req, res) => {
-  Renter.findById(req.params.renterId).then(renter => {
-    if( !renter ) {
-      return res.status(400).send({
-        message: "Renter by this ID:" + req.params.renterId + " is not found"
-      });
-    }
-    res.send(renter);
-  }).catch(err => {
-    return res.status(500).send({
-      message: "Error retrieving renter with ID " + req.params.renterId
-    });
-  });
+exports.findOne = (req, res) => {  
+  dbOperation.findOneById(Renter, req, res);
 };
 
 //Update info about renter
@@ -69,39 +43,10 @@ exports.update = (req, res) => {
   }
 
   // Find renter and update it with the request body
-  Renter.findByIdAndUpdate(req.params.renterId, {
-    name: req.body.name,
-    adress: req.body.adress,
-    reminders: req.body.reminders,
-    comments: req.body.comments
-  }, { new: true })
-  .then(renter => {
-    if(!renter) {
-      return res.status(404).send({
-        message: "Renter not found with id " + req.params.renterId
-      });
-    }
-    res.send(renter);
-  }).catch(err => {
-    return res.status(500).send({
-      message: "Error updating renter with id " + req.params.renterId
-    });
-  });
+  dbOperation.updateById(Renter, req, res);
 };
 
 //Delete renter by ID
 exports.delete = (req, res) => {
-  Renter.findByIdAndRemove(req.params.renterId)
-  .then(renter => {
-    if(!renter) {
-      return res.status(404).send({
-        message: "Renter not found with id " + req.params.renterId
-      });
-    }
-    res.send({message: "Renter deleted successfully!"});
-  }).catch(err => {
-    return res.status(500).send({
-      message: "Could not delete renter with id " + req.params.renterId
-    });
-  });
+  dbOperation.deleteById(Renter, req, res);
 };
--- app/db/db.js
+++ app/db/db.js
@@ -6,6 +6,66 @@ module.exports.saveDB = function (renter, res) {
       message: err.message
     });
   });
+}
 
-  console.log("Data is saved");
+module.exports.findAllEntrys = function(Renter, res) {
+  Renter.find().then(renters => {
+    res.send(renters);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message
+    });
+  });
+}
+
+module.exports.findOneById = function (Renter, req, res) {
+  Renter.findById(req.params.renterId).then(renter => {
+    if( !renter ) {
+      return res.status(400).send({
+        message: "Renter by this ID:" + req.params.renterId + " is not found"
+      });
+    }
+    res.send(renter);
+  }).catch(err => {
+    return res.status(500).send({
+      message: "Error retrieving renter with ID " + req.params.renterId
+    });
+  });
+}
+
+module.exports.updateById = function (Renter, req, res) {
+  Renter.findByIdAndUpdate(req.params.renterId, {
+    name: req.body.name,
+    adress: req.body.adress,
+    reminders: req.body.reminders,
+    comments: req.body.comments
+  }, { new: true })
+  .then(renter => {
+    if(!renter) {
+      return res.status(404).send({
+        message: "Renter not found with id " + req.params.renterId
+      });
+    }
+    res.send(renter);
+  }).catch(err => {
+    return res.status(500).send({
+      message: "Error updating renter with id " + req.params.renterId
+    });
+  });
+}
+
+module.exports.deleteById = function() {
+  Renter.findByIdAndRemove(req.params.renterId)
+  .then(renter => {
+    if(!renter) {
+      return res.status(404).send({
+        message: "Renter not found with id " + req.params.renterId
+      });
+    }
+    res.send({message: "Renter deleted successfully!"});
+  }).catch(err => {
+    return res.status(500).send({
+      message: "Could not delete renter with id " + req.params.renterId
+    });
+  });
 }
